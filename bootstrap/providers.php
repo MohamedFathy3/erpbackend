18008ad32c0f4e7aa5956cc869c08fd4c78791c4
MohamedFathy3 <1.53659521e+08+MohamedFathy3@users.noreply.github.com>
2026-09-09 11:58:16 +0000

    remove telescope provider from production bootstrap

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,4 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
-    ...(class_exists(\Laravel\Telescope\TelescopeApplicationServiceProvider::class)
-        ? [App\Providers\TelescopeServiceProvider::class]
-        : []),
 ];
