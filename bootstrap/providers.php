498cfb9aace93c3479a474a891a02c6f71b0c96f
MohamedFathy3 <1.53659521e+08+MohamedFathy3@users.noreply.github.com>
2026-09-09 11:34:33 +0000

    make telescope optional in production deployments

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,5 +3,7 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\RepositoryServiceProvider::class,
-    App\Providers\TelescopeServiceProvider::class,
+    ...(class_exists(\Laravel\Telescope\TelescopeApplicationServiceProvider::class)
+        ? [App\Providers\TelescopeServiceProvider::class]
+        : []),
 ];
